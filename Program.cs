5264b6f67026e0ed3a852da872b06c1720b78a89
Bruno <brunovinicius082005@gmail.com>
2021-09-30 08:52:15 -0300

    estrutura

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace EstruturaRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = string.Empty;
+
+           string [] processad = new string[1];
+
+           processad[0] = "Processador :";
+
+
+           string [] placam = new string[1];
+
+           placam[0] = "placa mãe :";
+
+            string [] font = new string[1];
+
+            font[0] = "Fonte:";
+
+             string [] memoriar = new string[1];
+
+           memoriar[0] = "Memoria ram :";
+
+            string [] memori = new string[1];
+
+           memori[0] = "Memoria :";
+           Console.WriteLine("-------------------------------------------------------");
+           Console.WriteLine("Digite o componente do computador que você queira saber");
+           n = Console.ReadLine();
+           Console.WriteLine("-------------------------------------------------------");
+
+           if (n == "processador")
+           {
+               for (int i = 0; i < n.Length; i++)
+               {
+                   Console.WriteLine($"{processad[i]} Um processador é uma espécie de microchip especializado. A sua função é acelerar, endereçar, resolver ou preparar dados, dependendo da aplicação.");
+               }
+           }
+
+           if (n == "placa mãe")
+           {
+               for (int i = 0; i < n.Length; i++)
+               {
+                   Console.WriteLine($"{placam[i]} A placa-mãe serve como uma plataforma única para conectar todas as peças de um computador. Uma placa-mãe se conecta A CPU, memória, discos rígidos, unidades ópticas, placa de vídeo, placa de som, e outras portas e placas de expansão directa ou através dos cabos. Ele pode ser considerado como a espinha dorsal de um computador. ");
+               }
+           }
+
+           if (n == "memoria ram ")
+           {
+               for (int i = 0; i < n.Length; i++)
+               {
+                   Console.WriteLine($"{memoriar[i]} A RAM é a memória de curto prazo de um computador. Em contraste, o HD ou o SDD de um computador é a memória de longo prazo, onde as coisas ficam armazenadas permanentemente ou até serem excluídas.");
+               }
+           }
+
+            if (n == "memoria")
+           {
+               for (int i = 0; i < n.Length; i++)
+               {
+                    Console.WriteLine($"{memori[i]} memória representa todos os dispositivos que podem armazenar informações, temporária ou permanentemente.");
+               }
+           }
+
+            if (n == "fonte")
+           {
+               for (int i = 0; i < n.Length; i++)
+                {
+                  Console.WriteLine($"{memori[i]} Uma fonte de alimentação é um equipamento usado para alimentar cargas elétricas. Cada dispositivo eletroeletrônico necessita de uma fonte para prover energia para seus componentes.")   
+                }
+           }
+        }
+    }
+}
